4528f3e55e13d5bbef3df53a46274d090abb9b4c
Avinash9111989 <65747243+Avinash9111989@users.noreply.github.com>
2026-05-07 23:24:04 +0530

    Add AuthResponse DTO

---
+++ common/src/main/java/com/banking/common/dto/user/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.banking.common.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+    private String token;
+    private String refreshToken;
+    private String tokenType;
+    private Long expiresIn;
+    private UserResponse user;
+}
