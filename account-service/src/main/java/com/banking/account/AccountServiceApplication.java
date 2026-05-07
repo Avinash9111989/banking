1834c9602af62a71d11b78d6439afa49e4b35a1c
Avinash9111989 <65747243+Avinash9111989@users.noreply.github.com>
2026-05-07 22:44:47 +0530

    Add Account Service Application class

---
+++ account-service/src/main/java/com/banking/account/AccountServiceApplication.java
@@ -0,0 +1,15 @@
+package com.banking.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.banking.account", "com.banking.common"})
+public class AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+}
