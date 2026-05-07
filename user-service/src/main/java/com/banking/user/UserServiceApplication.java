6ba8eb2f25296eab9d2992e54f9f8c9101ec8869
Avinash9111989 <65747243+Avinash9111989@users.noreply.github.com>
2026-05-07 23:36:59 +0530

    Add User Service Application class

---
+++ user-service/src/main/java/com/banking/user/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.banking.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.banking.user", "com.banking.common"})
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
